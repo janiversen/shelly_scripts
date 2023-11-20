37702140d22a7e184ccaa4c065538b172f240727
jan iversen <jancasacondor@gmail.com>
2023-11-20 11:15:34 +0100

    Final, all switches identical.

--- script_piso.js
+++ script_piso.js
@@ -4,7 +4,7 @@
 // 2 = Bedroom, bedroom door
 // 3 = Bedroom, bed belen
 // 4 = Bedroom, bed jan
-let location = 1;
+let location = 0;
 
 
 
@@ -12,33 +12,25 @@ function reserve() {
 };
 
 
-function executeLight(inx) {
-  execute(light_ip[inx] + '/relay/0?turn=toggle');
-}
 
 function lightBed() {
-  executeLight(1);
+  execute(CONFIG.light_ip[1] + '/relay/0?turn=toggle');
 }
 
 function lightRoof() {
-  executeLight(0);
+  execute(CONFIG.light_ip[0] + '/relay/0?turn=toggle');
 }
 
 
-
-function executeLed(inx) {
-  let base0 = CONFIG.leds_ip[inx] + '/white/';
-  let cmd = "?turn=toggle";
-  execute(base0 + "0" + cmd);
-  execute(base0 + "1" + cmd);
-}
 
 function ledSalon() {
-  executeLed(0);
+  execute(CONFIG.leds_ip[0] + '/white/0?turn=toggle');
+  execute(CONFIG.leds_ip[0] + '/white/1?turn=toggle');
 }
 
 function ledBedroom() {
-  executeLed(1);
+  execute(CONFIG.leds_ip[1] + '/white/0?turn=toggle');
+  execute(CONFIG.leds_ip[1] + '/white/1?turn=toggle');
 }
 
 
@@ -72,90 +64,42 @@ function blinds() {
 };
 
 
-//  let base0 = CONFIG.leds_ip[loc] + '/white/';
-//  let cmd = "?turn=on&brightness=";
-//  execute(base0 + "0" + cmd + level[0]);
-//  execute(base0 + "1" + cmd + level[1]);
-
-
-function led_regulate_intensity(loc, direction, brightness) {
-  print("JAN --> " + brightness[0] + " -- " + brightness[1]);
-  if (direction > 0) {
-    brightness[0] += 10
-    if (brightness[0] > 100) {
-      brightness[1] += 10
-      brightness[0] = 0
-      if (brightness[1] > 100) {
-        direction = -1
-        brightness[0] = 100
-        brightness[1] = 90
-      }
-    }
+function led_steps(ud) {
+  let level = CONFIG.led_cycles[ud[1]];
+  let loc = CONFIG.leds_ip[ud[0]]
+  execute(loc + '/white/0?turn=on&brightness=' + level[0]);
+  execute(loc + '/white/1?turn=on&brightness=' + level[1]);
+  ud[1]++;
+  if (ud[1] >= CONFIG.led_cycles.length) {
+    ud[1] = 0
   }
-  else {
-    brightness[0] -= 10
-    if (brightness[0] < 0) {
-      brightness[1] -= 10
-      brightness[0] = 100
-      if (brightness[1] < 0) {
-        direction = 1
-        brightness[0] = 0
-        brightness[1] = 0
-      }
-    }
-  }
-  print("JAN set --> " + brightness[0] + " -- " + brightness[1]);
 }
 
-let pushTimer = null;
-let led_direction = 1;
-let led_brightness = [0, 0]
-
-function led_intensity(loc, chan) {
-  Shelly.call(
-    "http.get", {
-      url: CONFIG.base_ip + CONFIG.leds_ip[loc] + '/white/' + chan
-    },
-    function (response) {
-      if (response.code === 200) {
-        let st = JSON.parse(response.body);
-        led_brightness[chan] = st.brightness
-      }
-      if (chan === 0) led_intensity(loc, 1)
-      else {
-        Timer.set(
-          CONFIG.timer_step,
-          true,
-          function (loc) {
-            print("JAN --> Timer ")
-            print("JAN direction " + led_direction)
-            print("JAN brightness 0 -> " + led_brightness[0] + "    1 -> " + led_brightness[0])
-            led_direction += 1;
-          }
-        )
-      }
-    }
+function led_intensity(loc) {
+  let cur_cycle = 0;
+  led_steps([loc, cur_cycle])
+  pushTimer = Timer.set(
+    CONFIG.timer_step,
+    true,
+    led_steps,
+    [loc, cur_cycle]
   );
-};
+}
 
 function ledPctSalon() {
-  led_intensity(0, 0);
+  led_intensity(0);
 }
 
 function ledPctBedroom() {
-  led_intensity(1, 0);
+  led_intensity(1);
 }
 
 
 
 function execute(command) {
   Shelly.call(
-      "http.get", {
-          url: CONFIG.base_ip + command
-      },
-      function (response, error_code, error_message, ud) {
-
-      },
+      "http.get", {url: CONFIG.base_ip + command},
+      function (response, error_code, error_message, ud) {},
       null
   );
 };
@@ -169,13 +113,14 @@ Shelly.addEventHandler(
 );
 
 
+let pushTimer = null;
 let CONFIG = {
   base_ip: "http://192.168.20.",
   blinds_ip: ["115", "113", "105"],
   leds_ip: ["117", "107"],
   light_ip: ["104", "108"],
 
-  timer_step: 500,
+  timer_step: 1000,
 
   // Keypad <-> Event id:
   // +---+---+
@@ -185,19 +130,20 @@ let CONFIG = {
   // +---+---+
 
   action_push: [
-    [reserve,    reserve,   blinds,     ledSalon],    // Standard
-    [reserve,    reserve,   ledSalon,   blinds],      // Left -> Right
-    [lightRoof,  lightBed,  blinds,     ledBedroom],  // Standard
-    [ledBedroom, blinds,    lightBed,   lightRoof],   // Left -> Right, Up -> Down
-    [lightBed,   lightRoof, ledBedroom, blinds],      // Left -> Right
+    [reserve,    reserve,    blinds,     ledSalon],    // Standard
+    [reserve,    reserve,    ledSalon,   blinds],      // Left -> Right
+    [lightRoof,  lightBed,   blinds,     ledBedroom],  // Standard
+    [blinds,     ledBedroom, lightRoof,  lightBed],   // Left -> Right, Up -> Down
+    [lightBed,   lightRoof,  ledBedroom, blinds],      // Left -> Right
   ],
   action_long_push: [
-    [reserve,       reserve, reserve,       ledPctSalon],    // Standard
-    [reserve,       reserve, ledPctSalon,   reserve],        // Left -> Right
-    [reserve,       reserve, reserve,       ledPctBedroom],  // Standard
-    [ledPctBedroom, reserve, reserve,       reserve],        // Left -> Right, Up -> Down
-    [reserve,       reserve, ledPctBedroom, reserve],        // Left -> Right
+    [reserve, reserve,       reserve,       ledPctSalon],    // Standard
+    [reserve, reserve,       ledPctSalon,   reserve],        // Left -> Right
+    [reserve, reserve,       reserve,       ledPctBedroom],  // Standard
+    [reserve, ledPctBedroom, reserve,       reserve],        // Left -> Right, Up -> Down
+    [reserve, reserve,       ledPctBedroom, reserve],        // Left -> Right
   ],
 
   blinds_stop: ["0", "40", "20"],
+  led_cycles: [["40", "50"], ["100", "100"], ["1", "20"]],
 };
