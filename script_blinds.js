b038c0e19267b13ba57d753dd6ac4e8c47662fb9
jan iversen <jancasacondor@gmail.com>
2026-03-02 22:06:40 +0100

    Allow blinds to fix open/close.

--- script_blinds.js
+++ script_blinds.js
@@ -1,22 +1,26 @@
 
-let blindsBajoDown = false
-let blindsEncimaDown = false
-
 function set_blindsBajo() {
-  set_blinds(CONFIG.bajo, blindsBajoDown)
-  blindsBajoDown = !blindsBajoDown;
+  set_blinds(CONFIG.bajo, CONFIG.bajo.state)
+  CONFIG.bajo.state = !CONFIG.bajo.state;
   return true
 };
 
 function set_blindsEncima() {
-  set_blinds(CONFIG.encima, blindsEncimaDown)
-  blindsEncimaDown = !blindsEncimaDown;
+  set_blinds(CONFIG.encima, CONFIG.encima.state)
   return true
+}
+
+function fix_blinds(do_salon, do_down) {
+  if (do_salon) updown = CONFIG.encima
+  else updown = CONFIG.bajo
+  updown.state = do_down
+  set_blinds(updown, do_down)
 }
 
 function set_blinds(updown, do_down) {
   if (do_down) blinds = updown.down;
   else blinds = updown.up;
+  updown.state = do_down
   for (var i = 0; i < 3; i++) {
     Shelly.call(
         "http.get", {url: blinds[i]}, null, null);
@@ -24,20 +28,10 @@ function set_blinds(updown, do_down) {
 }
 
 
-Shelly.addEventHandler(
-  function (event, user_data) {
-    if (event["info"]["component"] !== "input:0") return; 
-    if (event["info"]["event"] === "single_push") {
-      set_blindsBajo();
-    }; 
-    if (event["info"]["event"] === "long_push") {
-      set_blindsEncima();
-    }; 
-  },
-);
 
 let CONFIG = {
   bajo: {
+    state: false,
     up: [
       "http://192.168.20.105/roller/0?go=open",
       "http://192.168.20.113/roller/0?go=open",
@@ -45,11 +39,13 @@ let CONFIG = {
     ],
     down: [
       "http://192.168.20.105/roller/0?go=close",
-      "http://192.168.20.113/roller/0?go=to_pos&roller_pos=58",
+      "http://192.168.20.113/roller/0?go=close",
+      // "http://192.168.20.113/roller/0?go=to_pos&roller_pos=58",
       "http://192.168.20.115/roller/0?go=close"
     ]
   },
   encima: {
+    state: false,
     up: [
       "http://192.168.20.133/roller/0?go=open",
       "http://192.168.20.139/roller/0?go=open",
