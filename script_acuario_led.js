57383d2f4aceef3c7b2ad404bd071e7d7e81b505
jan iversen <jancasacondor@gmail.com>
2026-03-04 17:10:04 +0100

    acuario led.

--- script_acuario_led.js
+++ script_acuario_led.js
@@ -1,31 +1,40 @@
 
+cancel_timer = null
+
 function toggle_led() {
-    console.log("Handling toggle")
-    Shelly.call("light.GetStatus", { id: 0 }, function (res, err, msg) {
-        console.log("got status")
-        if (res.output === true) {
-            led_off()
-        } else {
-            led_on()
-        }
-    });    
+    if (cancel_timer) Timer.clear(cancel_timer)
+    const status = Shelly.getComponentStatus("Light:1");
+    if (status.output) led_off()
+    else led_on()
     return "Ok"
 }
 
 function led_on() {
-    console.log("Light is OFF, start.");
     Shelly.call("Light.set", { id: 0, on: true });
     Shelly.call("Light.set", { id: 1, on: true });
     Shelly.call("Light.set", { id: 2, on: true });
     Shelly.call("Light.set", { id: 3, on: true });
-    console.log("Light is OFF, turning it on.");
 }
 
 function led_off() {
-    console.log("Light is ON, start.");
-    Shelly.call("Light.set", { id: 0, on: false });
     Shelly.call("Light.set", { id: 1, on: false });
-    Shelly.call("Light.set", { id: 2, on: false });
+    cancel_timer = Timer.set(
+        2 * 60 * 1000,
+        false,
+        led_off2       
+    )
+}
+
+function led_off2() {
     Shelly.call("Light.set", { id: 3, on: false });
-    console.log("Light is ON, turning it off.");
+    cancel_timer = Timer.set(
+        50 * 60 * 1000,
+        false,
+        led_off_night        
+    )
+}
+
+function led_off_night() {
+    Shelly.call("Light.set", { id: 0, on: false });
+    Shelly.call("Light.set", { id: 2, on: false });
 }
